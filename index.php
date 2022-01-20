72012ea69d872855a4a2afff058d2676a4f73778
VictorSantos567 <victoraugustos49@gmail.com>
2022-01-20 15:51:30 -0300

    Modularizando paginas PHP

--- index.php
+++ index.php
@@ -7,7 +7,27 @@ include 'db.php';
 include 'header.php';
 
 # Conteúdo da página
+# pegando o conteudo da pagina, @ para ignorar
+# se a variavel pagina existir, pegue a pagina
+# senao pagina é home
+if(isset($_GET['pagina'])){
+    $pagina = $_GET['pagina'];
+}
+else{
+    $pagina == 'home';
+}
+if($pagina == 'cursos'){
+    include 'views/cursos.php';
+}
+elseif($pagina == 'alunos'){
+    include 'views/alunos.php';
+}
+elseif($pagina == 'matriculas'){
+    include 'views/matriculas.php';
+}
+else{
 include 'views/home.php';
+}
 
 # Rodapé
 include 'footer.php';
